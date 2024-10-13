ae72b9a882d65e11c41b79e15f4d32759b96c9b8
Myros27 <Myros27@users.noreply.github.com>
2024-10-13 18:17:45 +0200

    Update myros.js

--- 1.5/utils/myros.js
+++ 1.5/utils/myros.js
@@ -1,3 +1,36 @@
 function getGl() {
     return Object.values(save.globalLevel).reduce((sum, value) => sum + value, 0);
 }
+
+function mapTypeToIcon(type){
+    const iconList = {
+        miningDamage: 'mdi-bomb',
+        currencyMiningScrapGain: 'mdi-dots-triangle',
+        miningOreGain: 'mdi-chart-bubble',
+        miningSmelterySpeed: 'mdi-fire',
+        currencyMiningSmokeGain: 'mdi-smoke',
+        currencyMiningCrystalGreenGain: 'mdi-star-three-points',
+        currencyMiningCrystalYellowGain: 'mdi-star-four-points',
+        queueSpeedVillageBuilding: 'mdi-hammer',
+        villageMaterialGain: 'mdi-tree',
+        currencyVillageCoinGain: 'mdi-circle-multiple',
+        villageMentalGain: 'mdi-brain',
+        currencyVillageFaithGain: 'mdi-hands-pray',
+        currencyVillageSharesGain: 'mdi-certificate',
+        hordeAttack: 'mdi-sword',
+        currencyHordeBoneGain: 'mdi-bone',
+        currencyHordeMonsterPartGain: 'mdi-stomach',
+        hordeItemMasteryGain: 'mdi-seal',
+        currencyHordeSoulCorruptedGain: 'mdi-ghost',
+        currencyFarmVegetableGain: 'mdi-carrot',
+        currencyFarmBerryGain: 'mdi-fruit-grapes',
+        currencyFarmGrainGain: 'mdi-barley',
+        currencyFarmFlowerGain: 'mdi-flower',
+        farmExperience: 'mdi-star',
+        currencyGalleryBeautyGain: 'mdi-image-filter-vintage',
+        currencyGalleryConverterGain: 'mdi-recycle',
+        currencyGalleryPackageGain: 'mdi-package-variant',
+        currencyGalleryCashGain: 'mdi-cash',
+    }
+    return iconList[type]
+}
