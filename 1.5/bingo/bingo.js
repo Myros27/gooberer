29dc24645c929e86e26cd2295ec0a5e3c4014a36
Severin <severinw27@gmail.com>
2024-10-30 13:23:36 +0100

    support

--- 1.5/bingo/bingo.js
+++ 1.5/bingo/bingo.js
@@ -69,7 +69,7 @@ function startCrunch(){
         supportThreads = 1
     }
     let threads = mainTreads + supportThreads
-    if (threads <= 0 || threads > 1000){
+    if (threads <= 0 || threads > 100){
         mainTreads = 1
     }
     let count = 0
